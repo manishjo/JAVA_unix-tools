06a9e191d1cab559d1fe5bedeebd20c89890e088
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2014-01-10 23:50:25 +0530

    head done

--- src/manishjo/unixtools/Head.java
+++ src/manishjo/unixtools/Head.java
@@ -1,5 +1,42 @@
 package manishjo.unixtools;
 
-public class Head {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+class showHead{
+    public String printHead(String data, int times){
+        String resultData = "";
+        String result[] = data.split("\n");
+        for(int i = 1;i<times;i++){
+            resultData = resultData+"\n"+result[i];
+        }
+        return resultData;
+    }
 }
+public class Head {
+     public static void main(String[] args) {
+          String data = "";
+          int times = 10;
+          try {
+                String sCurrentLine;
+                if(args.length == 1){
+                    BufferedReader br = new BufferedReader(new FileReader(args[0]));
+                    while ((sCurrentLine = br.readLine()) != null)
+                        data = data+"\n"+sCurrentLine;
+                }
+                else{
+                    times = Integer.parseInt(args[0])+1;
+                    BufferedReader br = new BufferedReader(new FileReader(args[1]));
+                    while ((sCurrentLine = br.readLine()) != null)
+                        data = data+"\n"+sCurrentLine;
+                }
+                showHead show_head = new showHead();
+                data = show_head.printHead(data,times);
+                System.out.println(data);
+            }
+          catch (Exception e) {
+                System.out.println("file not found");
+            }
+        }
+    }
+
