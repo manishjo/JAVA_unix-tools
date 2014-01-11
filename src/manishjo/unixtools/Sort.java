3a1cee9ecc676503fab16cd4204a048cff1e43cd
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2014-01-11 11:58:16 +0530

    started sort

--- src/manishjo/unixtools/Sort.java
+++ src/manishjo/unixtools/Sort.java
@@ -1,9 +1,20 @@
 package manishjo.unixtools;
 
+import java.util.Arrays;
+
 /**
  * Created by manishjo on 1/11/14.
  */
 
 public class Sort {
-
+    public String showSortedData(String data){
+        String lines[] = data.split("\n");
+        String sortedData = null;
+        Arrays.sort(lines);
+        for(int i=0;i<lines.length;i++)
+            sortedData += lines[i];
+        return sortedData;
+    }
 }
+
+
