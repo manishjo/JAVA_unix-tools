1f415f7576cc23fe92825c27629f9130fd507eba
manish <manishjo@INmanishjo.corporate.thoughtworks.com>
2014-01-11 10:55:37 +0530

    refactored uniq class

--- src/manishjo/unixtools/Uniq.java
+++ src/manishjo/unixtools/Uniq.java
@@ -15,37 +15,27 @@ class removeRedundantLines {
                     list.add(temp[index-1]);
             }
             list.add(temp[index-1]);
-            for ( int i = 0 ; i < list.size() ; i ++){
+            for ( int i = 0 ; i < list.size() ; i ++)
                 result += list.get(i)+"\n";
-            }
             return result.substring(0,result.length()-1);
         }
  }
 
 public class Uniq {
     public static void main(String[] args) {
-        String data = "";
+        String data = null;
         int times = 10;
-        try {
-            String sCurrentLine;
-            if(args.length == 1){
-                BufferedReader br = new BufferedReader(new FileReader(args[0]));
-                while ((sCurrentLine = br.readLine()) != null)
-                    data = data+"\n"+sCurrentLine;
-            }
-            else{
-                times = Integer.parseInt(args[0])+1;
-                BufferedReader br = new BufferedReader(new FileReader(args[1]));
-                while ((sCurrentLine = br.readLine()) != null)
-                    data = data+"\n"+sCurrentLine;
-            }
-            removeRedundantLines results = new removeRedundantLines();
-            data = results.removeCommonLines(data);
-            writeFile fileWriter = new writeFile();
-            fileWriter.write_in_File(args[0],data);
+        String sCurrentLine;
+        fileOperations fs = new fileOperations();
+        if(args.length == 1)
+            data = fs.readFile(args[0]);
+        else{
+            times = Integer.parseInt(args[0])+1;
+            data = fs.readFile(args[1]);
         }
-        catch (Exception e) {
-            System.out.println("file not found");
-        }
+        removeRedundantLines results = new removeRedundantLines();
+        data = results.removeCommonLines(data);
+        writeFile fileWriter = new writeFile();
+        fileWriter.write_in_File(args[0],data);
     }
 }
